7dc80dc0a56aa588fdef57d1a92e6b2fe6240c25
Shrena Patel <shrena.p@hotmail.co.uk>
2022-02-18 10:10:28 +1300

    get hardcoded state displaying

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,10 @@
 import React from 'react'
+import Books from './Books'
 
 function App () {
   return (
     <>
-      <header className="header">
-        <h1>My Collection</h1>
-      </header>
-      <section className="main">
-        <h2>Books:</h2>
-        <p>Title:</p>
-        <p>Author:</p>
-        <img src="images/book-placeholder.jpeg"/>
-        <p>Release year:</p>
-      </section>
+      <Books />
     </>
   )
 }
--- client/components/Books.jsx
+++ client/components/Books.jsx
@@ -2,19 +2,16 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 
 function Books () {
-  const state = useSelector(state => state.Books)
-  console.log(state)
+  const state = useSelector(state => state.bookReducer)
+  console.log(state[0])
   return (
     <>
-      <header className="header">
-        <h1>My Collection</h1>
-      </header>
       <section className="main">
-        <h2>Books:</h2>
-        <p>Title:</p>
-        <p>Author:</p>
-        <img src="images/book-placeholder.jpeg"/>
-        <p>Release year:</p>
+        <h2>Books: </h2>
+        <p>Title: {state[0].title}</p>
+        <p>Author: {state[0].author} </p>
+        `<img src="/images/${state}"/>`
+        <p>Release year: {state[0].year_released} </p>
       </section>
     </>
   )
