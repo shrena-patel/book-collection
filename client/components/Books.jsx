28628fee9db7f312dcb405f50c54a29e7caafe30
Shrena Patel <shrena.p@hotmail.co.uk>
2022-02-18 14:16:44 +1300

    image showing from hardcoded state

--- client/components/Books.jsx
+++ client/components/Books.jsx
@@ -4,13 +4,14 @@ import { useSelector } from 'react-redux'
 function Books () {
   const state = useSelector(state => state.bookReducer)
   console.log(state[0])
+
   return (
     <>
       <section className="main">
         <h2>Books: </h2>
         <p>Title: {state[0].title}</p>
         <p>Author: {state[0].author} </p>
-        `<img src="/images/${state}"/>`
+        <img className="bookImage" src={state[0].cover}/>
         <p>Release year: {state[0].year_released} </p>
       </section>
     </>
