00e14fdd91d73072061eac5f45569387c00f175c
Shrena Patel <shrena.p@hotmail.co.uk>
2022-11-29 09:15:36 +1300

    remove commented out code

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -8,7 +8,6 @@ function Nav () {
   const menuRef = useRef(null)
   const burgerRef = useRef(null)
   const modalRef = useRef(null)
-  // const closeModalRef = useRef(null)
 
   const handleToggleMenu = () => {
     if (menuRef.current.style.display !== 'block') {
@@ -26,17 +25,12 @@ function Nav () {
 
   const handleModal = () => {
     const modalClassNames = modalRef.current.className
-    // console.log(modalClassNames.includes('boo'), 'MODAL')
     if (!modalClassNames.includes('is-active')) {
       modalRef.current.className = 'modal is-active'
     } else {
       modalRef.current.className = 'modal'
     }
   }
-
-  // const closeModalProp = () => {
-  //   console.log('close modal success')
-  // }
 
   return (
     <>
@@ -54,7 +48,6 @@ function Nav () {
         <div ref={menuRef} id="navbarExampleTransparentExample" className="navbar-menu">
           <div className="navbar-start">
             <Link to='/' className='navbar-item' onClick={hideMenu}>Home</Link>
-            {/* <Link to='/addbook' className='navbar-item' onClick={hideMenu}>Add book</Link> */}
             <p className='navbar-item js-modal-trigger' data-target="modal-js-example" onClick={handleModal}>Add book</p>
             <div className="navbar-item has-dropdown is-hoverable">
               <a className="navbar-link" href="">
@@ -67,7 +60,6 @@ function Nav () {
                 <a className="navbar-item" href="https://catalogue.wcl.govt.nz/?section=myaccount&page=myLists&tab=myList_682563133">
             Reading list
                 </a>
-                {/* <hr className="navbar-divider"/> */}
               </div>
             </div>
           </div>
@@ -93,9 +85,6 @@ function Nav () {
                     <span>Borrow a book</span>
                   </a>
                 </p>
-                {/* <button className="js-modal-trigger button is-primary" data-target="modal-js-example" onClick={handleModal}>
-                  Open JS example modal
-                </button> */}
               </div>
             </div>
           </div>
@@ -106,7 +95,6 @@ function Nav () {
         <div className="modal-content">
           <div className="box">
             <AddBook/>
-            {/* <!-- Your content --> */}
           </div>
         </div>
         <button className="modal-close is-large" aria-label="close" onClick={handleModal}></button>
